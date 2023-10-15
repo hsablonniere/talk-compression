d7b07b7e4e6862eae32972c6e4f5938ad338da70
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2023-10-15 21:54:46 +0200

    slides: tip v3

--- src/js/slide-types/slide-tip.js
+++ src/js/slide-types/slide-tip.js
@@ -10,9 +10,9 @@ defineSlideType('slide-tip', {
       <div class="scrabble-board">
         <img src="/src/img/scrabble-empty.svg" alt="">
       </div>
-      <div class="background"></div>
-      <div class="tip-number">${number}</div>
-      <div class="tip-text">${tip}</div>
+      <div class="background">
+        <div class="tip-text">${tip}</div>
+      </div>
     `;
   },
   // language=CSS
@@ -28,14 +28,11 @@ defineSlideType('slide-tip', {
 
     :host {
       --nb-half-size: 3rem;
-      position: relative;
       display: grid;
-      grid-template-columns: 1fr var(--nb-half-size) var(--nb-half-size) 2fr;
-      grid-template-rows: 1fr 1fr;
+      grid-template-columns: 1fr 2fr;
     }
 
     .scrabble-board {
-      grid-area: 1 / 1 / 3 / 3;
     }
 
     img {
@@ -49,33 +46,19 @@ defineSlideType('slide-tip', {
 
     .background {
       background-color: #2f6646;
-      grid-area: 1 / 3 / 3 / 5;
       z-index: 2;
+      display: flex;
     }
 
-    .tip-number,
     .tip-text {
       color: #fff;
       font-family: 'Interstate', sans-serif;
     }
 
-    .tip-number {
-      grid-area: 1 / 2 / 2 / 4;
-      font-size: 3.5rem;
-      background-color: #ee2325;
-      text-align: center;
-      font-family: 'Sufler', sans-serif;
-      z-index: 3;
-      align-self: end;
-      height: calc(var(--nb-half-size) * 2);
-      line-height: calc(var(--nb-half-size) * 2);
-    }
-
     .tip-text {
-      align-self: start;
-      padding: 3rem;
+      margin: auto;
+      padding: 0 3rem;
       box-sizing: border-box;
-      grid-area: 2 / 3 / 3 / 5;
       font-size: 2.5rem;
       line-height: 1.4;
       z-index: 3;
