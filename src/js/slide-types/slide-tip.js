1c1e82cf0da13b202dc656564bf09a1def9e5122
Antoine Caron <acaron@scaleway.com>
2024-04-25 11:34:42 +0200

    update qrcode for feedbacks

--- src/js/slide-types/slide-tip.js
+++ src/js/slide-types/slide-tip.js
@@ -20,7 +20,7 @@ defineSlideType('slide-tip', {
     return html`
       <img class="scrabble-board" src="/src/img/scrabble-empty.svg" alt="">
       <div class="background">
-        <img class="qrcode" src="/src/img/qr-code.png" alt="">
+        <img class="qrcode" src="/src/img/qrcode.svg" alt="">
         ${tips.map((tip) => html`
           <div class="tip-text">${tip}</div>
         `)}
